c8c704526b39534976d56b45f55a78dcdd3459a4
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-07-17 13:56:41 -0600

    july 17th - assignment 12 jake bird

--- ByuiDo/src/byui/cit260/byuiDo/view/SaveGameView.java
+++ ByuiDo/src/byui/cit260/byuiDo/view/SaveGameView.java
@@ -17,11 +17,8 @@ class SaveGameView extends View{
     @Override
     public String[] getInputs() {
         String[] inputs = new String[1];
-        System.out.println("****************************************************");
-        System.out.println("* Save the game by pressing S                      *\n"
-                         + "* or return by pressing Q                          *");
-        System.out.println("****************************************************");
-         String menuOption = this.getInput("\nPlease enter the Menu Option");
+        this.console.println("To save the game, enter a valid file name");
+        String menuOption = this.getInput("\nPlease enter the Menu Option");
         inputs[0] = menuOption;
         return inputs;
     }
@@ -29,9 +26,11 @@ class SaveGameView extends View{
     @Override
     public boolean doAction(String[] inputs) {
     String filePath = inputs[0];
+    
     Game game = ByuiDo.getCurrentGame();
     
     try {
+        
         
     }
     }
